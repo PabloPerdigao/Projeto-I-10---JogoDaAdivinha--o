ce5f1d04a968a99f6e80fa70fc62ac37b644cd3a
Pablo Perdigão <pabloagps@gmail.com>
2024-05-16 06:41:38 -0300

    Refatoração do código

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
-const screen1 = document.querySelector(".screen1")
+// Variáveis
+  const screen1 = document.querySelector(".screen1")
+  const screen2 = document.querySelector(".screen2")
+  const btnTry = document.querySelector("btnTry")
+  const btnReset = document.querySelector("btnReset")
+  let randomNumber = Math.round(Math.random() * 10)
+  let xAttempts = 1;
 
-const screen2 = document.querySelector(".screen2")
+// Eventos
+  btnTry.addEventListener('click', handleTryClick)
+  btnReset.addEventListener('click', handleResetClick)
+  document.addEventListener('keydown', function(e){
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+      handleResetClick
+    }
+  }) 
 
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1;
+// funções callback
+  function handleTryClick(event) {
+    event.preventDefault() // não execute o padrão (não envia o formulário)
 
+    const inputNumber = document.querySelector("#inputNumber")
 
-// função callback
-function handleTryClick(event) {
-  event.preventDefault() // não execute o padrão (não envia o formulário)
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen()
+      screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
+    }
 
-  const inputNumber = document.querySelector("#inputNumber")
-
-  if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
-  
-    document.
-      querySelector(".screen h2")
-      .innerText = `acertou em ${xAttempts} tentativas`
+    inputNumber.value = "" 
+    // após a tentaiva sem sucesso,limpar o campo numérico das mesmas.
+    xAttempts++
   }
 
-  inputNumber.value = "" 
-  // após a tentaiva sem sucesso,limpar o campo numérico das mesmas.
-
-  xAttempts++
-}
-
-// Eventos 
-const btnTry = document.querySelector("btnTry")
-const btnReset = document.querySelector("btnReset")
+  function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+  }
 
 
-btnTry.addEventListener('click', handleTryClick)
-
-handleTryClick('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-  screen1.classList.add("hide")
-  screen2.classList.remove("hide")
-})
+  function toggleScreen () {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+  }
